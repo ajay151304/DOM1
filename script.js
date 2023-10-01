3c4f75276e096c9449b38b813dad77a7e615e068
ajay151304 <ajaypup25@gmail.com>
2023-10-02 00:56:22 +0530

    With Tag Name

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ Make the 3 rd element in the list have green background color
 Make all the elements in the list have bold color font
 */
 
-const items = document.getElementsByClassName("list-group-item");
+// const items = document.getElementsByClassName("list-group-item");
 
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+// }
+/* Add a new li element without the same class Name 
+And try editing it with getelementsbyclassname and then by getelementbytagname
+*/
+
+const li = document.getElementsByTagName("li");
+
+li[2].style.backgroundColor = "green";
+
+for (let i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
 }
